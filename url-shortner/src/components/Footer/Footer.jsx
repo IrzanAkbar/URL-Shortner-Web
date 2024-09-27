635fb5ea62d7dc2ffbc67ca4ef57b57451d0b3e3
Andi Irzan <serarechannel@gmail.com>
2024-09-27 13:14:42 +0800

    Update Footer.jsx

--- url-shortner/src/components/Footer/Footer.jsx
+++ url-shortner/src/components/Footer/Footer.jsx
@@ -2,27 +2,25 @@ import React from 'react'
 import './Footer.css'
 
 const Footer = () => {
-///social links
-  const linkedin='https://www.linkedin.com/in/anurag-prajapati34/'
-  const github='https://github.com/anurag-prajapati34'
-  const twitter='https://x.com/anurag_x34'
-
+  // social links
+  const instagram = 'https://www.instagram.com/zannonly_/'
+  const tiktok = 'https://www.tiktok.com/@zan.ae'
+  const whatsapp = 'https://wa.me/6285340827717'
 
   return (
     <div className='footer-container '>
 
-<p><i>Developed & Designed by</i> <b>@Anurag prajapati</b></p>
+      <p><i>Developed & Designed by</i> <b>@Anurag prajapati</b></p>
 
+      {/*my social accounts */}
+      <div className='socials'>
 
-{/*my social accounts */}
-<div className='socials'>
+        <div onClick={() => window.open(instagram, '_blank')} className='social-icon'><i class="fa-brands fa-instagram"></i></div>
+        <div onClick={() => window.open(tiktok, '_blank')} className='social-icon'><i class="fa-brands fa-tiktok"></i></div>
+        <div onClick={() => window.open(whatsapp, '_blank')} className='social-icon'><i class="fa-brands fa-whatsapp"></i></div>
+        <div onClick={() => window.open('mailto:prajapatianurag73240@gmail.com')} className='social-icon'><i class="fa-solid fa-envelope"></i></div>
 
-<div onClick={()=>window.open(linkedin,'_blank')} className='social-icon'><i   class="fa-brands fa-linkedin "></i></div>
-<div onClick={()=>window.open(github,'_blank')} className='social-icon'><i   class="fa-brands fa-github "></i></div>
-<div onClick={()=>window.open(twitter,'_blank')} className='social-icon'><i   class="fa-brands fa-x-twitter"></i></div>
-<div onClick={()=>window.open('mailto:prajapatianurag73240@gmail.com')} className='social-icon'><i class="fa-solid fa-envelope"></i></div>
-
-</div>
+      </div>
 
     </div>
   )
